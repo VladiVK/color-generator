7d694ac24ae46c22d0106deb4399dfd695c8338e
VladiVK <kozobon@gmail.com>
2020-11-17 17:57:26 +0200

    install values.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+// https://github.com/noeldelgado/values.js
 
 function App() {
   return (
---
+++ src/SingleColor.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SingleColor = () => {
+    return (
+        <div>
+            <h2>Single Color</h2>
+        </div>
+    )
+}
+
+export default SingleColor
